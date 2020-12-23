7fd769ef65f81df06b2622756b47fe0ab3415b50
laijianyi <sal597685816@live.cn>
2020-12-23 14:41:11 +0800

    init

---
+++ resources/lang/zh_CN/log.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Author: jyiL
+ * Date: 2020/12/23
+ * Time: 14:12
+ * Email: avril.leo@yahoo.com
+ */
+
+return [
+    'title' => '操作日志',
+    'setting_title' => '操作日志',
+];
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '1.0.0' => [
+        '操作日志记录',
+        'create_opration_log_table.php',
+    ],
+];
